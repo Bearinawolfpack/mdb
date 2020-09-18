66beb10cfb29ba62d249e5fd51db1a3fff80d8aa
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-17 21:07:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Movie from "./Movie.js";
+
+const movies = [
+	{
+		id: 1,
+		title: "Star Wars",
+	},
+	{
+		id: 2,
+		title: "Spiderman",
+	},
+	{
+		id: 3,
+		title: "36th Chamber of Shaolin",
+	},
+	{
+		id: 4,
+		title: "5 Deadly Venoms",
+	},
+];
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+				</header>
+				{movies.map((movie) => (
+					<Movie movie={movie} key={movie.id} />
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
