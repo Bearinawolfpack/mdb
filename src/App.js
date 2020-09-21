424ebe8940dbbe003000c77852f72a0ca083660b
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-21 10:50:51 -0400

    Added initial routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable-line/no-did-mount-set-state: 0 */
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import MoviesList from './MoviesList';
@@ -9,7 +9,9 @@ const App = () => (
 	<Router>
 		<div className="App">
 			<header className="App-header">
+			<Link to="/">
 				<img src={logo} className="App-logo" alt="logo" />
+			</Link>
 			</header>
 			<Switch>
 				<Route exact path="/" component={MoviesList} />
